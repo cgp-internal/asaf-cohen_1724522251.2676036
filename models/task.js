54642b61d6d7a5f9bd04f0f5622babb54ff4d635
cgp-client <code.gen.pro.system@gmail.com>
2024-08-24 20:57:45 +0300

    Adding models/task.js

---
+++ models/task.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  dueDate: {
+    type: Date,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+const taskModel = mongoose.model('Task', taskSchema);
+
+module.exports = taskModel;
